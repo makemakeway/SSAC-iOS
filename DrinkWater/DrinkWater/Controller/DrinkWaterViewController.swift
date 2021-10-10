e239882f2577b0f56e5d42bad6ff6253cb5a55b9
yeonpark <makemakeway@gmail.com>
2021-10-10 23:53:47 +0900

    텍스트필드 값 입력 시 초기화되는 이슈 수정 완료

--- DrinkWater/DrinkWater/Controller/DrinkWaterViewController.swift
+++ DrinkWater/DrinkWater/Controller/DrinkWaterViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 박연배 on 2021/10/08.
 //
 
+import SnapKit
 import UIKit
 
 class DrinkWaterViewController: UIViewController {
@@ -49,6 +50,7 @@ class DrinkWaterViewController: UIViewController {
     
     // 초기화 버튼을 눌렀을 때
     @IBAction func resetButtonClicked(_ sender: UIBarButtonItem) {
+        self.view.endEditing(true)
         
         let alert = UIAlertController(title: nil, message: "오늘의 기록이 초기화됩니다. 진행하시겠습니까?", preferredStyle: .alert)
         let okButton = UIAlertAction(title: "확인", style: .default) { _ in
@@ -63,7 +65,7 @@ class DrinkWaterViewController: UIViewController {
             self.imageUpdate()
             print("초기화 완료!")
         }
-        let cancelButton = UIAlertAction(title: "취소", style: .default, handler: nil)
+        let cancelButton = UIAlertAction(title: "취소", style: .default)
         
         alert.addAction(okButton)
         alert.addAction(cancelButton)
@@ -73,6 +75,7 @@ class DrinkWaterViewController: UIViewController {
     
     // 물 마시기 버튼을 눌렀을 때
     @IBAction func drinkWater(_ sender: UIButton) {
+        print("drinkWater Call")
         guard let water = drinkTextField.text else {
             return
         }
@@ -99,6 +102,7 @@ class DrinkWaterViewController: UIViewController {
     }
     
     func headerLabelsConfig() {
+        print("headerLabelsConfig call")
         feelingLabel.font = UIFont.systemFont(ofSize: 24, weight: .medium)
         feelingLabel.textColor = .white
         
@@ -114,6 +118,7 @@ class DrinkWaterViewController: UIViewController {
     
     // 목표에 따라 이미지 뷰의 이미지 변경
     func imageUpdate() {
+        print("imageUpdate call")
         if Int((progress * 100).rounded()) < 30 {
             self.mainImage.image = UIImage(named: "1-1")
         }
@@ -145,6 +150,7 @@ class DrinkWaterViewController: UIViewController {
     
     
     func progressUpdate() {
+        print("progressUpdate call")
         progress = Float(totalWater) / Float(needToDrink)
         
         progressLabel.text = "목표의 \(Int((progress * 100).rounded()))%"
@@ -152,30 +158,38 @@ class DrinkWaterViewController: UIViewController {
     
     
     func drinkTextFieldConfig() {
+        print("drinkTextFieldConfig call")
         self.drinkTextField.keyboardType = .numberPad
     }
     
     
     func feelingLabelUpdate() {
+        print("feelingLabelUpdate")
         if Int((progress * 100).rounded()) == 0 {
             feelingLabel.text = "목이 마르지 않으신가요?"
+            feelingLabel.textColor = .white
         }
         else if Int((progress * 100).rounded()) < 50 {
             feelingLabel.text = "잘하셨어요!"
+            feelingLabel.textColor = .white
         }
         else if Int((progress * 100).rounded()) < 100 {
             feelingLabel.text = "훌륭해요!"
+            feelingLabel.textColor = .white
         }
         else {
             feelingLabel.text = "완벽해요!"
+            feelingLabel.textColor = UIColor.orange
         }
     }
     
     func recommendedLabelConfig() {
+        print("recommendedLabelConfig")
         recommendedDrinking.text = "\(nickname)님의 하루 물 권장 섭취량은 \(Double(needToDrink) / 1000.0)L입니다."
     }
     
     func fetchUserInfo() {
+        print("fetchUserInfo")
         guard let data = UserDefaults.standard.value(forKey: "UserInfo") as? Data else {
             print("data is missing")
             return
@@ -193,35 +207,38 @@ class DrinkWaterViewController: UIViewController {
     }
     
     func fetchTodayWater() {
+        print("fetchTodayWater")
         let water = UserDefaults.standard.integer(forKey: "todayWater")
         
         self.todayDrinking.text = String(water) + "ml"
     }
     
     func completeTodayDrink() {
+        print("completeTodayDrink")
         self.feelingLabel.textColor = UIColor.red
-    }
-    
-    func calculateKeyboardHeight() {
-        
     }
     
     //MARK: Objc func
     
     @objc
     func keyboardWillShow(_ sender: Notification) {
-        
-        
-        if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-            let keyboardRectangle = keyboardFrame.cgRectValue
-            keyboardHeight = keyboardRectangle.height
-            print(keyboardHeight)
+        if keyBoardState == false {
+            if let keyboardFrame: NSValue = sender.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+                let keyboardRectangle = keyboardFrame.cgRectValue
+                keyboardHeight = keyboardRectangle.height
+                print(keyboardHeight)
+            }
+            print("KEYBOARD APPEAR")
+            drinkTextField.frame.origin.y -= keyboardHeight
+            recommendedDrinking.frame.origin.y -= keyboardHeight
+            drinkButton.frame.origin.y -= keyboardHeight
+            
+            for view in self.view.subviews {
+                view.translatesAutoresizingMaskIntoConstraints = true
+            }
+            
+            self.keyBoardState = true
         }
-        print("KEYBOARD APPEAR")
-        drinkTextField.frame.origin.y -= keyboardHeight
-        recommendedDrinking.frame.origin.y -= keyboardHeight
-        self.keyBoardState = true
-        
     }
     
     @objc
@@ -235,6 +252,12 @@ class DrinkWaterViewController: UIViewController {
         print("KEYBOARD Hide")
         drinkTextField.frame.origin.y += keyboardHeight
         recommendedDrinking.frame.origin.y += keyboardHeight
+        drinkButton.frame.origin.y += keyboardHeight
+        
+        for view in self.view.subviews {
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
         self.keyBoardState = false
         
     }
@@ -256,26 +279,37 @@ class DrinkWaterViewController: UIViewController {
         gesture.delegate = self
         view.addGestureRecognizer(gesture)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         fetchUserInfo()
         fetchTodayWater()
-        
         progressUpdate()
-        
         recommendedLabelConfig()
+        print("viewWillappear")
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
     
-    override func viewDidLayoutSubviews() {
-        feelingLabelUpdate()
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+        self.view.endEditing(true)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         imageUpdate()
+        feelingLabelUpdate()
     }
-    
     
 }
 
@@ -283,13 +317,18 @@ class DrinkWaterViewController: UIViewController {
 
 extension DrinkWaterViewController: UITextFieldDelegate {
 
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 0.3) {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing")
+        UIView.animate(withDuration: 0.2) {
             self.mainImage.frame.origin.y = -500
         }
+        return true
     }
     
+    
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
         self.drinkTextField.resignFirstResponder()
         UIView.animate(withDuration: 0.3) {
             self.mainImage.frame.origin.y = self.mainImagePosition!
@@ -299,10 +338,10 @@ extension DrinkWaterViewController: UITextFieldDelegate {
 
 extension DrinkWaterViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-        if touch.view == self.drinkTextField {
+        if touch.view == self.drinkTextField || touch.view == self.drinkButton {
             return false
         }
-        self.drinkTextField.resignFirstResponder()
+        self.view.endEditing(true)
         return true
     }
 }
